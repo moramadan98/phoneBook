1d1c9412d83f6d6f8c0f7a6c2d8e30f579e06178
Muhammad Gamal <47898196+Mu7ammadGamal@users.noreply.github.com>
2021-01-08 01:34:28 +0200

    Update search_string_in_buffer.asm
    If Found
    print (string), return r8 =1
    
    Else
    return r8 =0

--- search_string_in_buffer.asm
+++ search_string_in_buffer.asm
@@ -1,15 +1,28 @@
 section .data
   
-loadedBuffer db "Amr", 10, "Sara", 10,"Gamal", 10,"Ramadan",10,"Amr",10,0
+loadedBuffer db "Amr", 10, "Sara", 10,"Gamal", 10,"Ramadan",10,0
 trimmedString times 16 db 0
-
+keyWord db "Amr",0
 
  
 section  .text
   global  main
 
 main:
+    mov rbp, rsp; for correct debugging
+    
+        
+        _Search_String_in_Buffer:      
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        ;Change: r8,r10,r11,rsi,rdi,rbx,rcx,rdx,al,bl
+        ;Return: iF Found r8 = 1 , Else r8 = 0       
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;Read Char by Char is here to trim (Step 1.1)
+        
         ;parameters
+        xor r8 ,r8
         xor r10,r10
         xor r11,r11        
         
@@ -37,25 +50,25 @@ main:
         ;r10 start pos
         ;r11 end pos    
      
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;Trim is here
+;;;;;;;;Trim is here (Step 1.2)       
         
-        
-        ;parameters
+        ;parameters      
         xor rsi,rsi
         xor rdx,rdx
         xor rdi,rdi
         
-        ;mov rsi, nam+index         ; rsi = start pos > [&Buffer + index of start]
+        ;mov rsi, nam+index             ;rsi = start pos > [&Buffer + index of start]
         add rsi,r10
         add rsi,loadedBuffer
         
         
-        ;mov rdx, nam+index          ; rdx = end pos ==> [&Buffer + index of end]  
+        ;mov rdx, nam+index             ;rdx = end pos ==> [&Buffer + index of end]  
         add rdx,r11
         add rdx,loadedBuffer     
         
-        mov rdi,trimmedString            ; rdi = Distnation buffer 
+        mov rdi,trimmedString           ;rdi = Distnation buffer 
         
         ;Trim string into buffer (trimmedString) to compare
         trimString:
@@ -82,40 +95,62 @@ main:
         ;mov eax, 4
         ;mov ebx, 1
         ;mov ecx, trimmedString
-        ;mov edx, 16     ;len_trimmedString
+        ;mov edx, 16                ;len_trimmedString
         ;int 80h
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;Compare is here 
-      
-    
+;;;;;;;;compare is here (Step 2) 
+        
+        ;parameters                  ;it's ok to overwrite on last used REG (Anoter Step)
+        xor rsi,rsi        
+        xor rdi,rdi
+        xor rdx, rdx                 ; rdx = 0 ==> (index i)
+        
+        mov rsi, keyWord             ; rsi = &keyWord
+        mov rdi, trimmedString       ; rdi = &trimmedString
+        
+        cmpString_loop:
+        mov al, [rsi + rdx]
+        mov bl, [rdi + rdx]
+        inc rdx
+        cmp al, bl                  ; compare two current characters
+        jne cmpString_notEqual      ; not equal
+        cmp al, 0                   ; at end?
+        je cmpString_equal          ; end of both strings
+        jmp cmpString_loop          ; equal so far
+        
+        cmpString_notEqual:          
+        mov r8, 0                   ; to indicate if not found till end of outer loop
+        jmp cmpString_exit          ; to clear Buffer before the next iteration of searching
+        
+        cmpString_equal:
+        mov r8, 1                   ; to indicate if found till end of outer loop        
+        jmp _Search_String_in_Buffer_exit    ; Found! ==> So, exit from _Search_String_in_Buffer  
+        cmpString_exit:    
+        
+        ; r8 = 1 Found
+        ; r8 = 0 Not Found
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;Clear Buffer
-        mov rdi,0  
-        clearBuffre_loop:        
+;;;;;;;;Clear Buffer (Step 3 ==> if not found  ==> r8 = 0)
+        ;call _clearBuffer:        
+        xor rdi,rdi  
+        clearBuffer_loop:        
         cmp BYTE[trimmedString +rdi],0
-        je clearBuffre_exit
+        je clearBuffer_exit
         mov BYTE[trimmedString +rdi],0
-        add rdi, 1
-        jmp clearBuffre_loop      
-        clearBuffre_exit:
-
-        
-        ;xor rcx,rcx     ; i        
-        ;cmp BYTE[trimmedString + rcx], 0
-        ;je exit
-        ;mov al,0    
-        ;mov BYTE[trimmedString + rcx],al
-        ;inc rcx 
-        ;exit:
+        inc rdi
+        jmp clearBuffer_loop      
+        clearBuffer_exit:        
         
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;Update before looping again
         mov r10,r11
-        add r10,1
+        inc r10
         jmp read_CHARbyCHAR_label1       
-        read_CHARbyCHAR_done:
+        read_CHARbyCHAR_done:        
+        _Search_String_in_Buffer_exit:
+        ;ret
         
         ;print the info 
         mov eax, 4
@@ -130,5 +165,4 @@ main:
         int   80h
 
 
-
-        
+       
