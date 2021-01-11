0765296857cd1ade02132e4f59060e55fb89985d
Muhammad Gamal <47898196+Mu7ammadGamal@users.noreply.github.com>
2021-01-11 04:26:14 +0200

    Update PhoneBook.asm
    Handling some test cases and added new option to erase all the contacts

--- PhoneBook.asm
+++ PhoneBook.asm
@@ -1,6 +1,6 @@
 section  .data
 
-msgMain db 13,10,"Choose one of the following Option by enter the option number  ",13,10,"1- Add new contact",13,10,"2- Display all contacts",13,10,"3- Search in contacts",13,10,"4- Add number to existing contact",13,10,"5- Delete one number from contact",13,10,"6- Delete contact ",13,10,"0- Exit ",13,10,">>", 0
+msgMain db 13,10,"Choose one of the following Option by enter the option number  ",13,10,"1- Add new contact",13,10,"2- Display all contacts",13,10,"3- Search in contacts",13,10,"4- Add number to existing contact",13,10,"5- Delete one number from contact",13,10,"6- Delete contact ",13,10,"7- Erase contacts ",13,10,"0- Exit ",13,10,">>", 0
 
 lenmsgMain equ $ - msgMain  
 
@@ -42,6 +42,9 @@ lenmsgNotfoundError equ $ - msgNotfoundError
 msgEnterError db 13, 10, "[X] Invalid input ", 13, 10,0
 lenmsgEnterError equ $ - msgEnterError
 
+msgExistError db 13, 10, "[X] Contact name is already exist ", 13, 10,0
+lenmsgExistError equ $ - msgExistError
+
 msgEnterNumber db 13, 10, "[!] Enter Number ", 13, 10,">>", 0
 lenmsgEnterNumber equ $ - msgEnterNumber
 
@@ -49,17 +52,17 @@ ContactsFile db "contacts.txt" ,0
 
 newline db 0ah
 
-seedFile db 'contacts.txt'		
+seedFile db "contacts.txt" ,0
 length equ $-seedFile
 
 section .bss
    user_input resb 2    ; user input
    user_input_length equ $- user_input
    
-   Cname  resb 20
+   Cname  resb 50
    Cname_len equ $ - Cname
    
-   Cnum   resb 20
+   Cnum   resb 50
    Cnum_len equ $ - Cnum
    
    temp  resb 1024
@@ -75,8 +78,7 @@ section .bss
 
 section .text
     global main
- main:
-  
+ main:  
  	;; sys_open(file, permissions)
 	mov	rax, 2		; sys_open
 	mov	rdi, seedFile	; file
@@ -84,7 +86,7 @@ section .text
 	syscall
 
 	;; File exists?
-	mov	rdx,0
+	mov	rdx,0          ; not exist if rdx>0
 	cmp	rdx,rax
 	jle	loop
 
@@ -102,20 +104,20 @@ section .text
  loop:  
 
          ;write
-         mov  rax, 4             ; sys_write
-         mov  rbx, 1             ; stdout
-         mov  rcx, msgMain           ; buffer
-         mov  rdx, lenmsgMain          ; length
+         mov  rax, 4                        ; sys_write
+         mov  rbx, 1                        ; stdout
+         mov  rcx, msgMain                  ; buffer
+         mov  rdx, lenmsgMain               ; length
          int  80h
          
          
        
    ;read
        
-         mov  rax, 3 ; sys_read
-         mov  rbx, 0 ; stdin
-         mov  rcx, user_input ; user input
-         mov  rdx, user_input_length ; max length
+         mov  rax, 3                        ; sys_read
+         mov  rbx, 0                        ; stdin
+         mov  rcx, user_input               ; user input
+         mov  rdx, user_input_length        ; max length
          int 80h
         
          
@@ -143,6 +145,9 @@ section .text
          cmp byte [rcx],54
          je  _DeleteContact
          
+         cmp byte [rcx],55
+         je _ResetPhoneBook
+         
          cmp byte [rcx],48
          je exit
      
@@ -160,10 +165,33 @@ _AddContact:
     mov rdx ,Cname_len  
     call _inputWithLength;(rsi = &Buffer, rdx = &len)://rcx = len of input message, Buffer = input
     
+    ; check if null input
+    cmp BYTE[Cname], 10     ; cuz we didnt clear last char that = new line = 10
+    je msgInvalid
+    
     mov rbx ,Cname
     call _clearLastChar;(rbx = &Buffer,rcx = len)
     
-    mov rdx ,rcx
+    mov r15, rcx            ; to store len of Cname
+    
+    
+    
+    ; check if contact exist
+    mov rbx ,ContactsFile
+    mov rdi , loadedFile
+    call  _readFile;(rbx = &FileName , rdi =&loadedFile )://rax = [file_descriptor]
+    
+    mov r12 ,loadedFile
+    mov r13 , Cname
+    mov r14 , temp
+    call _Search_String_in_Buffer; (r12 = loadedBuffer, r13 = keyWord, r14 = trimmedString)://r8 = 1/0 Found/Not 
+    
+    cmp r8 , 1
+    je msgExist
+    
+    mov rcx,r15
+    ;Else
+    mov rdx ,r15
     mov rbx , ContactsFile
     mov rcx ,Cname
     call _appendInFile;(rbx = &FileName, rcx = &msg, rdx = Number of Bytes)://rax = [file_descriptor] 
@@ -184,6 +212,10 @@ _AddContact:
     mov rdx ,Cnum_len  
     call _inputWithLength;(rsi = &Buffer, rdx = &len)://rcx = len of input message, Buffer = input
     
+    ;check if null input
+    cmp BYTE[Cnum], 10          ; cuz we didnt clear last char that = new line = 10 
+    je msgInvalid
+  
   ; mov rbx ,Cnum
    ; call _clearLastChar;(rbx = &Buffer,rcx = len)
     
@@ -197,7 +229,7 @@ _AddContact:
      mov rbx , Cname
     call _clearBuffer;(rbx = &Buffer)
     
-     mov rbx , Cnum
+    mov rbx , Cnum
     call _clearBuffer;(rbx = &Buffer)
     
     jmp msgDone
@@ -239,11 +271,16 @@ _Search:
     mov rdx ,Cname_len  
     call _inputWithLength;(rsi = &Buffer, rdx = &len)://rcx = len of input message, Buffer = input
     
+    ; check if null input
+    cmp BYTE[Cname], 10     ; cuz we didnt clear last char that = new line = 10
+    je msgInvalid
+        
     mov rbx ,Cname
-    call _clearLastChar;(rbx = &Buffer,rcx = len)
+    call _clearLastChar;(rbx = &Buffer,rcx = len)   
     
     
-     mov rbx ,ContactsFile
+    ;Else    
+    mov rbx ,ContactsFile
     mov rdi , loadedFile
     call  _readFile;(rbx = &FileName , rdi =&loadedFile )://rax = [file_descriptor]
     
@@ -263,6 +300,9 @@ _Search:
     mov rsi ,newline
     mov rdx ,1
     call _print;(rsi = &Buffer, rdx = &len)
+    
+    mov rbx , temp
+    call _clearBuffer;(rbx = &Buffer)
     
     mov rbx ,Cname
     mov rdi , temp
@@ -298,9 +338,32 @@ _AddNumber:
     mov rdx ,Cname_len  
     call _inputWithLength;(rsi = &Buffer, rdx = &len)://rcx = len of input message, Buffer = input
     
+    ; check if null input
+    cmp BYTE[Cname], 10     ; cuz we didnt clear last char that = new line = 10
+    je msgInvalid
+    
     mov rbx ,Cname
     call _clearLastChar;(rbx = &Buffer,rcx = len)
     
+    mov r15, rcx            ; to store len of Cname    
+    
+    
+    ; check if contact exist
+    mov rbx ,ContactsFile
+    mov rdi , loadedFile
+    call  _readFile;(rbx = &FileName , rdi =&loadedFile )://rax = [file_descriptor]
+    
+    mov r12 ,loadedFile
+    mov r13 , Cname
+    mov r14 , temp
+    call _Search_String_in_Buffer; (r12 = loadedBuffer, r13 = keyWord, r14 = trimmedString)://r8 = 1/0 Found/Not 
+    
+    cmp r8 , 1
+    jne _NotFound
+    
+    mov rcx,r15             ; to restore len of Cname
+    ;Else
+        
     mov rsi ,msgEnterNumber
     mov rdx ,lenmsgEnterNumber
     call _print;(rsi = &Buffer, rdx = &len)
@@ -308,6 +371,11 @@ _AddNumber:
     mov rsi ,Cnum
     mov rdx ,Cnum_len  
     call _inputWithLength;(rsi = &Buffer, rdx = &len)://rcx = len of input message, Buffer = input
+    
+    ;check if null input
+    cmp BYTE[Cnum], 10          ; cuz we didnt clear last char that = new line = 10 
+    je msgInvalid
+    
     
     ;mov rbx ,Cnum
     ;call _clearLastChar;(rbx = &Buffer,rcx = len)
@@ -341,10 +409,35 @@ _DeleteNumber:
      mov rsi ,Cname
     mov rdx ,Cname_len  
     call _inputWithLength;(rsi = &Buffer, rdx = &len)://rcx = len of input message, Buffer = input
-      
-       mov rbx ,Cname
+    
+      ; check if null input
+    cmp BYTE[Cname], 10     ; cuz we didnt clear last char that = new line = 10
+    je msgInvalid  
+    
+    mov rbx ,Cname
     call _clearLastChar;(rbx = &Buffer,rcx = len)
     
+    mov r15, rcx            ; to store len of Cname      
+    
+    
+    ; check if contact exist
+    mov rbx ,ContactsFile
+    mov rdi , loadedFile
+    call  _readFile;(rbx = &FileName , rdi =&loadedFile )://rax = [file_descriptor]
+    
+    mov r12 ,loadedFile
+    mov r13 , Cname
+    mov r14 , temp
+    call _Search_String_in_Buffer; (r12 = loadedBuffer, r13 = keyWord, r14 = trimmedString)://r8 = 1/0 Found/Not 
+    
+    cmp r8 , 1
+    jne _NotFound
+    
+     mov rcx,r15             ; to restore len of Cname
+     mov rbx , temp
+     call _clearBuffer;(rbx = &Buffer)     
+     
+    ;Else    
     mov rbx ,Cname
     mov rdi , temp
     call  _readFile;(rbx = &FileName , rdi =&loadedFile )://rax = [file_descripto
@@ -362,10 +455,12 @@ _DeleteNumber:
     mov rdx ,Cnum_len  
     call _inputWithLength;(rsi = &Buffer, rdx = &len)://rcx = len of input message, Buffer = input
       
-       mov rbx ,Cnum
+    mov rbx ,Cnum
     call _clearLastChar;(rbx = &Buffer,rcx = len)
-    
-    
+        
+    ;check if null input
+    cmp BYTE[Cnum], 0          
+    je msgInvalid
     
     mov rbx , temp
     call _getBufferSize;(rbx = &Buffer):// rdi = length
@@ -408,8 +503,8 @@ _DeleteNumber:
     call _clearBuffer;(rbx = &Buffer)
 
     jmp msgDone
-    
- ;-----------------------------------------------------------------------------------------------------------------------------------------------   
+ 
+;-----------------------------------------------------------------------------------------------------------------------------------------------   
 _DeleteContact:
     mov  rax, 4             ; sys_write
     mov  rbx, 1             ; stdout
@@ -424,6 +519,24 @@ _DeleteContact:
     mov rbx ,Cname
     call _clearLastChar;(rbx = &Buffer,rcx = len)
     
+    ; check if contact exist
+    mov rbx ,ContactsFile
+    mov rdi , loadedFile
+    call  _readFile;(rbx = &FileName , rdi =&loadedFile )://rax = [file_descriptor]
+    
+    mov r12 ,loadedFile
+    mov r13 , Cname
+    mov r14 , temp
+    call _Search_String_in_Buffer; (r12 = loadedBuffer, r13 = keyWord, r14 = trimmedString)://r8 = 1/0 Found/Not 
+    
+    cmp r8 , 1
+    jne _NotFound
+    
+     mov rcx,r15             ; to restore len of Cname
+     mov rbx , temp
+     call _clearBuffer;(rbx = &Buffer)     
+     
+    ;Else   
 
      mov rbx ,ContactsFile
     mov rdi , loadedFile
@@ -467,8 +580,27 @@ _DeleteContact:
     call _clearBuffer;(rbx = &Buffer)
     jmp msgDone
  
-  ;-----------------------------------------------------------------------------------------------------------------------------------------------   
+;-----------------------------------------------------------------------------------------------------------------------------------------------   
+_ResetPhoneBook:
     
+    mov rbx ,ContactsFile
+    mov rdi , loadedFile
+    call  _readFile;(rbx = &FileName , rdi =&loadedFile )://rax = [file_descriptor]
+    
+    mov r12 ,loadedFile    
+    mov r14 , temp 
+    call _resetPhoneBook; (r12 = loadedBuffer, r14 = trimmedString)
+    
+    mov rbx , loadedFile
+    call _clearBuffer;(rbx = &Buffer)
+    mov rbx , temp
+    call _clearBuffer;(rbx = &Buffer)
+    
+    jmp msgDone_main
+        
+ 
+;-----------------------------------------------------------------------------------------------------------------------------------------------   
+  
   _NotFound:  
     mov  rax, 4             ; sys_write
     mov  rbx, 1             ; stdout
@@ -485,7 +617,25 @@ msgDone:
     call _print;(rsi = &Buffer, rdx = &len)
     jmp end
     
- exit:
+msgDone_main:
+    mov  rsi, msgDisplayDone           ; buffer
+    mov  rdx, lenmsgDisplayDone         ; length
+    call _print;(rsi = &Buffer, rdx = &len)
+    jmp main        ; to create new contacts file 
+    
+msgInvalid:
+    mov  rsi, msgEnterError           ; buffer
+    mov  rdx, lenmsgEnterError        ; length
+    call _print;(rsi = &Buffer, rdx = &len)
+    jmp end
+    
+msgExist:
+    mov  rsi, msgExistError           ; buffer
+    mov  rdx, lenmsgExistError        ; length
+    call _print;(rsi = &Buffer, rdx = &len)
+    jmp end
+    
+exit:
          mov    rax, 1
          mov    rbx, 0
          int    80h
@@ -548,6 +698,7 @@ _inputWithLength:;(rsi = &Buffer, rdx = &len)://rcx = len of input message, Buff
         sub rax,rsi                     ; subtract message from eax to get the difference from the byte that was 0 and the start (string length)
         leave
         
+        xor rcx,rcx
         mov rcx,rax                     ; store counter in rcx
 
 ret
@@ -806,8 +957,131 @@ _Search_String_in_Buffer:; (r12 = loadedBuffer, r13 = keyWord, r14 = trimmedStri
         _Search_String_in_Buffer_exit:
 ret
 ;-------------------------------------------------------------------------------------
+_resetPhoneBook:; (r12 = loadedBuffer, r14 = trimmedString)       
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        ;Change: r8,r10,r11,r12,r13,rsi,rdi,rbp,rbx,rcx,rdx,al,bl
+        ;Return: iF Found r8 = 1 , Else r8 = 0       
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        ;mov r12, rsi         ;temp reg to store &trimmedString (will change before its stage) 
+        ;mov r13, rdi        ;temp reg to store &keyWord  (will change before its stage) 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;Read Char by Char is here to trim (Step 1.1)      
+ 
+        
+        ;parameters
+        xor r8 ,r8
+        xor r10,r10
+        xor r11,r11        
+        
+        read_CHARbyCHAR_loop1_reset:
+        cmp byte [r12+r11],0
+        je read_CHARbyCHAR_done_reset
+        
+        cmp byte [r12+r11],10
+        je read_CHARbyCHAR_update_reset
+        
+        read_CHARbyCHAR_label1_reset:
+         
+        ;Print each trimmed name (only TEST)
+        ;mov     eax,  4 ;print 
+        ;mov     ebx,  1 ; 
+        ;lea     ecx,  [r12+r11]
+        ;mov     edx,  1
+        ;int     80h
+        
+        ;increment the loop counter
+        inc r11   
+        jmp read_CHARbyCHAR_loop1_reset        
+        read_CHARbyCHAR_update_reset:  
+        
+        ;r10 start pos
+        ;r11 end pos    
+     
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;Trim is here (Step 1.2)       
+        
+        ;parameters      
+        xor rsi,rsi
+        xor rdx,rdx
+        xor rdi,rdi
+        
+        ;mov rsi, nam+index             ;rsi = start pos > [&Buffer + index of start]
+        add rsi,r10
+        add rsi,r12
+        
+        
+        ;mov rdx, nam+index             ;rdx = end pos ==> [&Buffer + index of end]  
+        add rdx,r11
+        add rdx,r12     
+        
+        mov rdi,r14           ;rdi = Distnation buffer 
+        
+        ;Trim string into buffer (r14) to compare
+        trimString_reset:
+        ;len = (end - start) ==> to ignore last char ($ or etc..)
+        mov rbx, rdx             
+        sub rbx, rsi
+        ;inc rbx        
+        
+        xor rcx,rcx     ; i=0        
+       ;for(i=0;i<len;i++)                       
+        trimLoop_reset:
+        cmp rcx,rbx                  
+        jge exit_trimString_reset
+        
+        xor rax,rax
+        ;des[i]=sor[i]
+        mov al, BYTE[rsi + rcx]   
+        mov BYTE[rcx + rdi],al
+        inc rcx
+        jmp trimLoop_reset        
+        exit_trimString_reset:     
 
+        ;Print each trimmed name (only TEST) 
+        ;mov eax, 4
+        ;mov ebx, 1
+        ;mov ecx, r14
+        ;mov edx, 16                ;len_trimmedString
+        ;int 80h
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;; delete is here 
+        
+        ;parameters                  ;it's ok to overwrite on last used REG (Anoter Step)
+        xor rsi,rsi        
+        xor rdi,rdi
+        xor rdx, rdx                 ; rdx = 0 ==> (index i)
+        
+        mov rdi,rbx                  ; store rbx for any next need
+        mov rbx, r14                 ; r14 = &trimmedString        
+        call _deleteFile;(rbx = &FileName)
+        mov rbx, rdi                 ; restore rbx for any next need
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;Clear Buffer (Step 3 ==> if not found  ==> r8 = 0)
+        ;call _clearBuffer:        
+        xor rdi,rdi  
+        clearBuffer_loop_reset:        
+        cmp BYTE[r14 +rdi],0
+        je clearBuffer_exit_reset
+        mov BYTE[r14 +rdi],0
+        inc rdi
+        jmp clearBuffer_loop_reset      
+        clearBuffer_exit_reset:        
+        
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;Update before looping again
+        mov r10,r11
+        inc r10
+        jmp read_CHARbyCHAR_label1_reset       
+        read_CHARbyCHAR_done_reset:        
+        _Search_String_in_Buffer_exit_reset:
+        mov rbx, seedFile
+        call _deleteFile;(rbx = &FileName)
+        
+ret
+;-------------------------------------------------------------------------------------
 _removeStringinBuffer:;(rbx = &Buffer, rsi = start pos., rdi = end pos.)
         ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
         ;Change: rax,rbx,rcx,rdx          
@@ -877,7 +1151,6 @@ _writeFile:;(rcx = &msg, rbx = [file_descriptor], rdx = Number of Bytes)
         int 80h                         ;call kernel   
 ret
 ;-------------------------------------------------------------------------------------
-
 
 _getBufferSize:;(rbx = &Buffer):// rdi = length
         ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
